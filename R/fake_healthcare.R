d32cdfbcea411a0ec29bb382e8d4bb47b443e341
Jon Wayland <jonwayland47@gmail.com>
2021-09-24 21:44:09 -0400

    making the fake_healthcare dataset available as an R data object as opposed to strictly through the `createDB` function

---
+++ R/fake_healthcare.R
@@ -0,0 +1,30 @@
+
+#' Fake Healthcare Data
+#'
+#' Health and income outcomes for 184 countries from 1960 to 2016. Also includes two character vectors, \code{oecd}
+#' and \code{opec}, with the names of OECD and OPEC countries from 2016.
+#'
+#' \itemize{
+#'  \item PatientID
+#'  \item Age
+#'  \item CC_Count Description
+#'  \item Risk_Count Description
+#' }
+#'
+#'
+#' @docType data
+#'
+#' @usage data(fake_healthcare)
+#'
+#' @format An object of class \code{"data.frame"}.
+#'
+#' @keywords datasets
+#'
+#' @aliases oecd opec
+#'
+#' @examples
+#' data(fake_healthcare)
+#' head(fake_healthcare)
+#' print(oecd)
+#' print(opec)
+"fake_healthcare"
