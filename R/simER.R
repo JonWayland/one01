81d0faff0b4b34d3ce96c9607ea348a206ae434a
Jon Wayland <jonwayland47@gmail.com>
2022-09-21 15:56:53 -0400

    including simER data

---
+++ R/simER.R
@@ -0,0 +1,25 @@
+
+#' Simulated ER Data
+#'
+#' Fictional healthcare dataset containing aggregated ER costs, visit counts, and severity.
+#'
+#' \itemize{
+#'  \item Cost
+#'  \item Number of ER Visits
+#'  \item Typical Severity Level
+#' }
+#'
+#'
+#' @docType data
+#'
+#' @usage data(simER)
+#'
+#' @format An object of class \code{"data.frame"}.
+#'
+#' @keywords datasets
+#'
+#'
+#' @examples
+#' data(simER)
+#' head(simER)
+"simER"
